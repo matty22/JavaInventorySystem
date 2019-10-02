64768610d3b93e6253751920048fd6bbd173dd20
Matt <matt.leonard.22@gmail.com>
2019-10-02 14:57:30 -0700

    Adds main screen gui

--- src/View_Controller/ProductController.java
+++ src/View_Controller/ProductController.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package View_Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Matty
- */
+
 public class ProductController implements Initializable {
 
     /**
