93d92796de8f2266a9bbee4026e6cfd1b4342f5c
Matt <matt.leonard.22@gmail.com>
2019-10-07 15:01:50 -0700

    roughs out inventory class

--- src/Model/Inventory.java
+++ src/Model/Inventory.java
@@ -1,14 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
-/**
- *
- * @author Matty
- */
+import javafx.collections.ObservableList;
+
+
 public class Inventory {
+    private ObservableList<Part> allParts;
+    private ObservableList<Product> allProducts;
     
+    public void addPart(Part newPart) {
+    
+    }
+    
+    public void addProduct(Product newProduct) {
+    
+    }
+    
+    // Commented until I have ObservableLists setup
+//    public Part lookupPart(int partId) {
+//        
+//    }
+//    
+//    public Product lookupProduct(int productId) {
+//    
+//    }
+//    
+//    public ObservableList<Part> lookupPart(String partName) {
+//        
+//    }
+//    
+//    public ObservableList<Product> lookupProduct(String productName) {
+//    
+//    }
+    
+    public void updatePart(int index, Part selectedPart) {
+    
+    }
+    
+    public void updateProduct(int index, Product selectedProduct) {
+    
+    }
+    
+    public void deletePart(Part selectedPart) {
+    
+    }
+    
+    public void deleteProduct(Product selectedProduct) {
+    
+    }
+    
+    public ObservableList<Part> getAllParts() {
+        return this.allParts;
+    }
+    
+    public ObservableList<Product> getAllProducts() {
+        return this.allProducts;
+    }
 }
