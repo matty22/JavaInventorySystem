1b73360d9e1a027ef6cf93d746e347ff4230f585
Matt <matt.leonard.22@gmail.com>
2019-10-08 14:50:13 -0700

    table partially populated properly

--- src/Model/InhousePart.java
+++ src/Model/InhousePart.java
@@ -3,20 +3,32 @@ package Model;
 
 public class InhousePart extends Part {
     private int machineId;
+    private String name;
+    private int stock;
+    private double price;
+    private int min;
+    private int max;
   
     // Constructor
     public InhousePart(int id, String name, double price, int stock, int min, int max) {
         super(id, name, price, stock, min, max);
+        this.machineId = id;
     }
-    
-    // Setter
+   
+    /**
+     * @return the machineId
+     */
+    public int getMachineId() {
+        return machineId;
+    }
+
+    /**
+     * @param machineId the machineId to set
+     */
     public void setMachineId(int machineId) {
         this.machineId = machineId;
     }
     
-    // Getter
-    public int getMachineId() {
-        return this.machineId;
-    }
+    
     
 }
--- src/Model/Part.java
+++ src/Model/Part.java
@@ -11,66 +11,97 @@ public abstract class Part {
     
     // Constructor
     public Part(int id, String name, double price, int stock, int min, int max) {
-        
-    }
-    
-    // Constructor for table view
-    public Part(int id, String name, int stock, double price) {
-    
-    }
-    
-    // Setters
-    public void setId(int id) {
         this.partID = id;
-    }
-    
-    public void setName(String name) {
         this.partName = name;
-    }
-    
-    public void setPrice(double price) {
         this.partPrice = price;
-    }
-    
-    public void setStock(int stock) {
         this.partStock = stock;
-    }
-    
-    public void setMin(int min) {
         this.partMin = min;
-    }
-    
-    public void setMax(int max) {
         this.partMax = max;
     }
     
-    public void setPrice(int max) {
-        this.partPrice = max;
+
+    /**
+     * @return the partID
+     */
+    public int getPartID() {
+        return partID;
     }
-    
-    // Getters
-    public int getId() {
-        return this.partID;
+
+    /**
+     * @param partID the partID to set
+     */
+    public void setPartID(int partID) {
+        this.partID = partID;
     }
-    
-    public String getName() {
-        return this.partName;
+
+    /**
+     * @return the partName
+     */
+    public String getPartName() {
+        return partName;
     }
-    
-    public double getPrice() {
-        return this.partPrice;
+
+    /**
+     * @param partName the partName to set
+     */
+    public void setPartName(String partName) {
+        this.partName = partName;
     }
-    
-    public int getStock() {
-        return this.partStock;
+
+    /**
+     * @return the partPrice
+     */
+    public double getPartPrice() {
+        return partPrice;
     }
-    
-    public int getMin() {
-        return this.partMin;
+
+    /**
+     * @param partPrice the partPrice to set
+     */
+    public void setPartPrice(double partPrice) {
+        this.partPrice = partPrice;
     }
-    
-    public int getMax() {
-        return this.partMax;
+
+    /**
+     * @return the partStock
+     */
+    public int getPartStock() {
+        return partStock;
     }
-    
+
+    /**
+     * @param partStock the partStock to set
+     */
+    public void setPartStock(int partStock) {
+        this.partStock = partStock;
+    }
+
+    /**
+     * @return the partMin
+     */
+    public int getPartMin() {
+        return partMin;
+    }
+
+    /**
+     * @param partMin the partMin to set
+     */
+    public void setPartMin(int partMin) {
+        this.partMin = partMin;
+    }
+
+    /**
+     * @return the partMax
+     */
+    public int getPartMax() {
+        return partMax;
+    }
+
+    /**
+     * @param partMax the partMax to set
+     */
+    public void setPartMax(int partMax) {
+        this.partMax = partMax;
+    }
+      
 }
