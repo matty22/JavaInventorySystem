253096c28cc21a362b8c1dcaf55ef9ede07ec7ea
Matt <matt.leonard.22@gmail.com>
2019-10-16 15:28:10 -0700

    adds part id search to main page

--- src/View_Controller/MainController.java
+++ src/View_Controller/MainController.java
@@ -59,7 +59,7 @@ public class MainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
        
-       partIdColumn.setCellValueFactory(new PropertyValueFactory<>("machineId"));
+       partIdColumn.setCellValueFactory(new PropertyValueFactory<>("partID"));
        partNameColumn.setCellValueFactory(new PropertyValueFactory<>("partName"));
        partInventoryColumn.setCellValueFactory(new PropertyValueFactory<>("partStock"));
        partPriceColumn.setCellValueFactory(new PropertyValueFactory<>("partPrice"));
